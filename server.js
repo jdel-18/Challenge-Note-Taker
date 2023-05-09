591d9b3075170c76c3fb1589b2e9e1d132ee4ff1
jdel-18 <joshuadelves1@yahoo.com>
2023-05-08 23:49:20 -0500

    added stuff

--- server.js
+++ server.js
@@ -1,43 +1,41 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uuid = require('uniqid');
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('./api/notes', (req, res) => {
-    fs.readFile('.db/db.json', 'utf8', (err, data) => {
-        console.err(err);
-        res.json(JSON.parse(data));
-    })
-})
-app.post('./api/notes', (req, res) => {
-    fs.readFile('.db/db.json', 'utf8', (err, data) => {
-        console.err(err);
-        const notes = JSON.parse(data);
-        req.body['uuid'] = uuid();
-        notes.push(req.body);
-        fs.writeFile('.db/db.json', JSON.stringify(notes), (err) =>{
-            console.err(err);
-            res.json(req.body);
-        })
-    })
-})
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    console.error(err);
+    res.json(JSON.parse(data));
+  });
+});
 
-app.get('notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './notes.html'))
-})
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    console.error(err);
+    const notes = JSON.parse(data);
+    req.body['uuid'] = uuid();
+    notes.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      console.error(err);
+      res.json(req.body);
+    });
+  });
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes.html'));
+});
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'))
-})
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}!`)
-);
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}!`));
